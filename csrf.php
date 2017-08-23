1445271757f0eab435181bd60a205b22325bb25d
jefferson-chaves <jefferson7chaves@gmail.com>
2017-08-23 20:55:43 -0300

    Comentários úteis acerca do uso exemplo

--- csrf.php
+++ csrf.php
@@ -1,7 +1,5 @@
 <?php
-
-$cadastro = 'http://localhost/seguranca/salvar_cadastro.php?nome=Jefferson+Chaves&idade=28&email=jef%40gmail.com&senha=123&salvar=Enviar+dados';
-
+  $cadastro = 'http://localhost/seguranca/salvar_cadastro.php?nome=Jefferson+Chaves&idade=28&email=jef%40gmail.com&senha=123&salvar=Enviar+dados';
 ?>
-
+<!-- simulando o acesso a uma imagem -->
 <img src="<?php echo $cadastro; ?>">
